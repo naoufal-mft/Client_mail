ad7ea0f26c1bdd6742faba054ea1a7e1c7457a37
naoufal-mft <naoufal.meftahi@insa.ueuromed.org>
2023-03-17 22:40:49 +0100

    Initial commit

---
+++ src/EmailClientLogin.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class EmailClientLogin extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load the FXML file for the main page
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainInterface.fxml"));
+        Parent root = loader.load();
+
+        // Set the scene
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
